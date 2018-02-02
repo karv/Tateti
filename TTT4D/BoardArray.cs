5d4dcebe7285780c157958121e4adecd27fe3a63
Edgar Carballo <karvayoEdgar@gmail.com>
2018-02-02 13:49:30 -0700

    Add methods to (de)convert int⁴ into int (for tile coordinates)

--- TTT4D/BoardArray.cs
+++ TTT4D/BoardArray.cs
@@ -15,6 +15,50 @@
 		/// <param name="w">w value.</param>
 		public Board this[int w] => _status[w];
 
+		/// <summary>
+		/// Gets the status of a tile.
+		/// </summary>
+		/// <param name="x">The x coordinate.</param>
+		/// <param name="y">The y coordinate.</param>
+		/// <param name="z">The z coordinate.</param>
+		/// <param name="w">The w coordinate.</param>
+		public int this[int x, int y, int z, int w] => _status[w][x, y, z];
+
+		/// <summary>
+		/// Converts bijectively coordinates in this board into a <c>int</c>.
+		/// </summary>
+		/// <seealso cref="Undigitalize"/>
+		/// <param name="x">The x coordinate.</param>
+		/// <param name="y">The y coordinate.</param>
+		/// <param name="z">The z coordinate.</param>
+		/// <param name="w">The w coordinate.</param>
+		public static int Digitalize(int x, int y, int z, int w)
+		{
+			const int base_2 = Board.BoardSize * Board.BoardSize;
+			const int base_3 = base_2 * Board.BoardSize;
+			const int base_4 = base_3 * Board.BoardSize;
+
+			return x + base_2 * y + base_3 * z + base_4 * w;
+		}
+
+		/// <summary>
+		/// Converts bijectively a <c>int</c> into coordinates.
+		/// </summary>
+		/// <param name="index">Index.</param>
+		public static (int x, int y, int z, int w) Undigitalize(int index)
+		{
+			(int x, int y, int z, int w) coords;
+			coords.x = index % Board.BoardSize;
+			index /= Board.BoardSize;
+			coords.y = index % Board.BoardSize;
+			index /= Board.BoardSize;
+			coords.z = index % Board.BoardSize;
+			index /= Board.BoardSize;
+			coords.w = index % Board.BoardSize;
+
+			return coords;
+		}
+
 		/// <summary/>
 		public BoardArray()
 		{ _status = new Board[Board.BoardSize]; }
