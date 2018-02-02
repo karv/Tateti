8bd2398d11ad6514ba25c312e0df3e10564dc2a7
Edgar Carballo <karvayoEdgar@gmail.com>
2018-02-02 14:07:09 -0700

    Formalize Point3 and Point4

---
+++ TTT4D/Point3.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TTT4D
+{
+	/// <summary>
+	/// A point in <c>int</c>³.
+	/// </summary>
+	public struct Point3 : IEquatable<Point3>
+	{
+		/// <summary>
+		/// The X coordinate.
+		/// </summary>
+		public int X { get; }
+		/// <summary>
+		/// The Y coordinate.
+		/// </summary>
+		public int Y { get; }
+		/// <summary>
+		/// The Z coordinate.
+		/// </summary>
+		public int Z { get; }
+
+		public Point3(int x, int y, int z)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+		}
+
+		/// <summary>
+		/// Compare to another <see cref="Point3"/>
+		/// </summary>
+		public bool Equals(Point3 other) => X == other.X && Y == other.Y && Z == other.Z;
+		/// <summary>
+		/// Compare to another <see cref="Point3"/>
+		/// </summary>
+		public override bool Equals(object obj) => obj is Point3 other && Equals(other);
+		/// <summary>
+		/// Serves as a hash function for a <see cref="T:TTT4D.Point3"/> object.
+		/// </summary>
+		public override int GetHashCode() => X ^ Y ^ Z;
+		/// <summary>
+		/// Compares two <see cref="Point3"/>
+		/// </summary>
+		public static bool operator ==(Point3 left, Point3 right) => left.Equals(right);
+		/// <summary>
+		/// Compares two <see cref="Point3"/>
+		/// </summary>
+		public static bool operator !=(Point3 left, Point3 right) => !(left == right);
+
+		public static Point3 Zero { get; }
+		static Point3()
+		{
+			Zero = new Point3(0, 0, 0);
+		}
+	}
+}
---
+++ TTT4D/Point4.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace TTT4D
+{
+	/// <summary>
+	/// A point in <c>int</c>⁴.
+	/// </summary>
+	public struct Point4 : IEquatable<Point4>
+	{
+		/// <summary>
+		/// The X coordinate.
+		/// </summary>
+		public int X { get; }
+		/// <summary>
+		/// The Y coordinate.
+		/// </summary>
+		public int Y { get; }
+		/// <summary>
+		/// The Z coordinate.
+		/// </summary>
+		public int Z { get; }
+		/// <summary>
+		/// The W coordinate.
+		/// </summary>
+		public int W { get; }
+
+		public Point4(int x, int y, int z, int w)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+			W = w;
+		}
+		/// <summary>
+		/// Compare to another <see cref="Point4"/>
+		/// </summary>
+		public bool Equals(Point4 other) => X == other.X && Y == other.Y && Z == other.Z && W == other.W;
+		/// <summary>
+		/// Compare to another <see cref="Point4"/>
+		/// </summary>
+		public override bool Equals(object obj) => obj is Point4 other && Equals(other);
+		/// <summary>
+		/// Serves as a hash function for a <see cref="Point4"/> object.
+		/// </summary>
+		public override int GetHashCode() => (X ^ Y) ^ (Z ^ W);
+		/// <summary>
+		/// Compares two <see cref="Point4"/>
+		/// </summary>
+		public static bool operator ==(Point4 left, Point4 right) => left.Equals(right);
+		/// <summary>
+		/// Compares two <see cref="Point4"/>
+		/// </summary>
+		public static bool operator !=(Point4 left, Point4 right) => !(left == right);
+
+		public static Point4 Zero { get; }
+		static Point4()
+		{
+			Zero = new Point4(0, 0, 0, 0);
+		}
+
+	}
+}
