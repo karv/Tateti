61267c27a2975c7561d2cb0056f0cf659ba6dfa5
Edgar Carballo <karvayoEdgar@gmail.com>
2018-02-02 10:21:11 -0700

    Add PlayerAction struct

---
+++ TTT4D/PlayerAction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TTT4D
+{
+	/// <summary>
+	/// Represents a move of a <see cref="Player"/> in a <see cref="Match"/>.
+	/// </summary>
+	public struct PlayerAction
+	{
+		/// <summary>
+		/// Gets the player who makes the move.
+		/// </summary>
+		public Player Actor { get; }
+		/// <summary>
+		/// Gets the x index of the move.
+		/// </summary>
+		public int X { get; }
+		/// <summary>
+		/// Gets the y index of the move.
+		/// </summary>
+		public int Y { get; }
+		/// <summary>
+		/// Gets the z index of the move.
+		/// </summary>
+		public int Z { get; }
+		/// <summary>
+		/// Gets the w index of the move.
+		/// </summary>
+		public int W { get; }
+
+		public PlayerAction(Player actor, int x, int y, int z, int w)
+		{
+			Actor = actor ?? throw new ArgumentNullException(nameof(actor));
+			X = x;
+			Y = y;
+			Z = z;
+			W = w;
+		}
+	}
+}
